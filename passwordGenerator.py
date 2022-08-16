8b9781c7da483acbace0e8dabf99f2a379d80ad4
tobyaskew1 <46008253+tobyaskew1@users.noreply.github.com>
2022-08-16 07:28:07 +0100

    passwordGenerator.py
    Here is the final product of my password generator, all fully working. Now to try and simplify it and shorten the code.

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -4,8 +4,9 @@ numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 alphabetCaps = []
 chars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", "|", ":", ";", "\"", "\'", "<", ">", ",", ".", "?", "/"]
-password = []
+password = ''
 num = 0
+selection = numbers + alphabetCaps + alphabet + chars
 
 for i in alphabet :
     alphabetCaps.append(i.upper())
@@ -13,17 +14,9 @@ for i in alphabet :
 passwordLength = int(input("Please enter the length of your password: "))
 
 while num < passwordLength:
-    password.append(random.choice(numbers))
+    str(selection) + password
+    #password.append(random.choice(selection))
     num += 1
-    while num < passwordLength:
-        password.append(random.choice(chars))
-        num += 1
-        while num < passwordLength:
-            password.append(random.choice(alphabetCaps))
-            num += 1
-            while num < passwordLength:
-                password.append(random.choice(alphabet))
-                num += 1
-
 random.shuffle(password)
+
 print(password)
