f51f0aeb7fdb0cd6a2af25258c0e377b649b2065
tobyaskew1 <46008253+tobyaskew1@users.noreply.github.com>
2022-08-18 07:37:10 +0100

    I have simplified the code massively.
    By using ASCII code I have managed to shorten and simplify the code by a significant amount.

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,23 +1,13 @@
 import random
+import string
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-alphabetCaps = []
-chars = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", "|", ":", ";", "\"", "\'", "<", ">", ",", ".", "?", "/"]
-password = ''
-string = ""
-num = 0
+password = ""
+selection = string.digits + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
 
-for i in alphabet :
-    alphabetCaps.append(i.upper())
-
-selection = numbers + alphabetCaps + alphabet + chars
-
+print("Welcome to Toby's password generator!")
 passwordLength = int(input("Please enter the length of your password: "))
 
-while num < passwordLength:
-    string = random.choice(selection)
-    password = str(string) + password
-    num += 1
+password = random.sample(selection, passwordLength)
 
-print(password)
+print("Your new password is : "+"".join(password))
+print("Don't share this password with anyone, but share the generator!")
